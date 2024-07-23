9e2365e6c5d699fc5bb1ff32b403ceb59a5626ab
cowardlycamper32 <g.hickman2022@gmail.com>
2024-07-23 17:03:00 +0100

    midway through adding items

--- src/main/java/net/cowardlycamper32/techent/item/ModItems.java
+++ src/main/java/net/cowardlycamper32/techent/item/ModItems.java
@@ -5,10 +5,16 @@ import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = 
             DeferredRegister.create(ForgeRegistries.ITEMS, TechnologicalEntanglement.MODID);
+    
+    public static final RegistryObject<Item> IMPURE_IRON_ORE = 
+            ITEMS.register("impure_iron_ore",
+                    () -> new Item(new Item.Properties()));
+    
     
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
